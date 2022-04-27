f0c9a7a26f8a6129522a85ab11aa0af2511e9995
Monisha Govindaraju <rg.monisha105@gmail.com>
2022-04-27 10:01:16 +0530

    CHARITY APP

---
+++ src/main/java/com/charityapp/CharityAppAPI/CharityAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.charityapp.CharityAppAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charityapp/CharityAppAPI/CharityAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charityapp.CharityAppAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
